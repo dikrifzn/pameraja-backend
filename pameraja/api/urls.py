e4fcc28ed05809877d915c8e72a5cb03e85f24f3
dikrifzn <dikrifauz@gmai.com>
2025-07-11 17:39:09 +0700

    feat(auth): add login, logout, and register API endpoints
    refactor: implement standardized success/error API responses via helper

--- pameraja/api/urls.py
+++ pameraja/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UserAPI, SocialMediaAPI, UserProfileAPI, ProjectAPI, CommentAPI, LikeAPI
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import UserAPI, SocialMediaAPI, UserProfileAPI, ProjectAPI, CommentAPI, LikeAPI, RegisterView, LogoutView
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('users/', UserAPI.as_view(), name='user-list'),
     path('users/<int:id>', UserProfileAPI.as_view(), name='user-profile'),
     path('social-media/<int:id>', SocialMediaAPI.as_view(), name='socialmedia-list'),
--- pameraja/pameraja/settings.py
+++ pameraja/pameraja/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'api.apps.ApiConfig',
 ]
 
@@ -125,6 +126,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100
 }
